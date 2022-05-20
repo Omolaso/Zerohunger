db3e9a57adfdf454f817b78c66f4fb73261b8ba8
omolaso <adextee001@gmail.com>
2022-05-20 22:44:35 +0100

    ZEROHUNGER

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const menuIcon = document.querySelector('#bars');
 menuIcon.addEventListener('click', () => {
     const nav = document.querySelector('.nav1' , '.nav2');
     console.log(nav)
-    nav.classList.toggle('list');
+    nav.classList.toggle('.list');
 })
